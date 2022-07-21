af4a3c37860e67ee3e6f8c67e9d74e8bc8f80936
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-07-21 10:16:38 +0200

    adjust footer text
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- daita/footer.py
+++ daita/footer.py
@@ -1,8 +1,7 @@
 import os
 
-
 def footer():
-    print("In case you encounter any issues or questions, please contact us at contact@daita.tech")
-    print("or raise issue at this repository https://github.com/daita-technologies/daita-python-library")
-    print("Best Regard!")
+    print("If you experience any issues or have any questions, please contact us at contact@daita.tech")
+    print("or open an issue here: https://github.com/daita-technologies/daita-python-library/issues")
+    print("✨ Your DAITA Team")
     os._exit(1)
