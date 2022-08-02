da76168e2087afd67786e949d514f4f5ede4cea7
BEdaita <daitaservices@gmail.com>
2022-08-02 14:38:44 +0000

    Dellete debug

--- daita/load_env.py
+++ daita/load_env.py
@@ -4,6 +4,4 @@ import os
 pwd = os.path.dirname(os.path.abspath(__file__))
 envfile = os.path.join(pwd, '.env.development')
 dotenv_path = Path(envfile)
-print(dotenv_path)
 load_dotenv(dotenv_path=dotenv_path)
-print(os.environ['DAITA_TOKEN'])
