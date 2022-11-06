c5a0672803aae2b225a442b57a5b9d2029673d4d
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-11-06 13:49:25 +0100

    ✨ fix formatting
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- daita/daita.py
+++ daita/daita.py
@@ -1,6 +1,6 @@
 from daita.dashboard import dashboard
 import argparse
-import signal 
+import signal
 
 parser = argparse.ArgumentParser(description="Optional app description")
 parser.add_argument("--dir", type=str, help="A required integer positional argument")
@@ -11,20 +11,22 @@ args = parser.parse_args()
 dir = args.dir
 daita_token = args.daita_token
 
-class KeyboardInterrupt:
 
+class KeyboardInterrupt:
     def __enter__(self):
         self.signal_received = False
         self.old_handler = signal.signal(signal.SIGINT, self.handler)
-                
+
     def handler(self, sig, frame):
         print("Please waitting processing finis, thank you.")
         self.signal_received = (sig, frame)
-    
+
     def __exit__(self, type, value, traceback):
         signal.signal(signal.SIGINT, self.old_handler)
         if self.signal_received:
             self.old_handler(*self.signal_received)
+
+
 def main():
     with KeyboardInterrupt():
         dashboard(daita_token=daita_token, dir=dir)
