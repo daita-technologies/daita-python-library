4649f97518d8784be2fb08f79aad964e1fbbb12b
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-11-06 12:51:07 +0100

    further check
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- daita/load_env.py
+++ daita/load_env.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 import os
 
-# pwd = os.path.dirname(os.path.abspath(__file__))
-# envfile = os.path.join(pwd, ".env")
-# dotenv_path = Path(envfile)
+pwd = os.path.dirname(os.path.abspath(__file__))
+envfile = os.path.join(pwd, ".env")
+dotenv_path = Path(envfile)
 load_dotenv()
