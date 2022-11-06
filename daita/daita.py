5d7d29ab97d1fd24cf58ec4355683611b0f93403
BEdaita <daitaservices@gmail.com>
2022-11-06 06:45:58 +0000

    Update: Handling of unexpected interruptions during upload

--- daita/daita.py
+++ daita/daita.py
@@ -1,6 +1,6 @@
 from daita.dashboard import dashboard
 import argparse
-
+import signal 
 
 parser = argparse.ArgumentParser(description="Optional app description")
 parser.add_argument("--dir", type=str, help="A required integer positional argument")
@@ -11,7 +11,21 @@ args = parser.parse_args()
 dir = args.dir
 daita_token = args.daita_token
 
+class DelayedKeyboardInterrupt:
 
+    def __enter__(self):
+        self.signal_received = False
+        self.old_handler = signal.signal(signal.SIGINT, self.handler)
+                
+    def handler(self, sig, frame):
+        print("Please waitting processing finis, thank you.")
+        self.signal_received = (sig, frame)
+    
+    def __exit__(self, type, value, traceback):
+        signal.signal(signal.SIGINT, self.old_handler)
+        if self.signal_received:
+            self.old_handler(*self.signal_received)
 def main():
-    dashboard(daita_token=daita_token, dir=dir)
-    pass
+    with DelayedKeyboardInterrupt():
+        dashboard(daita_token=daita_token, dir=dir)
+        pass
