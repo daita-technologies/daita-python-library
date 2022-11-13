42d353661695009303fdec62ba466b1e3037c072
BEdaita <daitaservices@gmail.com>
2022-11-13 14:39:13 +0700

    update: progress bar for zip

--- daita/upload_zip.py
+++ daita/upload_zip.py
@@ -1,10 +1,19 @@
 import os
 import requests
+import time
+from tqdm import tqdm
 from daita.footer import footer
 
 endpointCreatePresignUrlSinglefile = os.environ["CREATE_PRESIGN_SIGNLE_URL"]
 endpointUploadCompressfile = os.environ["UPLOAD_COMPRESS_FILE"]
+endpointTaskProgress = os.environ['TASK_PROGRESS']
 
+def get_task(id_token,task_id):
+    responseTaskProgress = requests.get(
+        endpointTaskProgress, params ={'id_token':id_token,'task_id':task_id}
+
+    )
+    return responseTaskProgress
 
 def upload_compress_file(filename, daita_token):
     resp = requests.post(
@@ -19,14 +28,37 @@ def upload_compress_file(filename, daita_token):
         data=preSignUrlResult["presign_url"]["fields"],
         files=files,
     )
-    responseTask = requests.post(
-        endpointUploadCompressfile, json={"s3": s3_uri, "daita_token": daita_token}
-    )
+    
+    responseTask = get_task(id_token,task_id)
     data = responseTask.json()
-    task_id = data["data"]["data"]["task_id"]
+    data = data["data"]["data"]
+    task_id = data["task_id"]
+    id_token = data['id_token']
+    
     print(data["message"])
     print(f"Check task id {task_id} on https://dev.daita.tech/my-tasks")
+    responseTaskProgress = get_task(id_token,task_id)
+    jsonTaskProgress = responseTaskProgress.json()
 
+    if jsonTaskProgress['body']['message'] != 'OK':
+        footer()
+    data = jsonTaskProgress['body']['data']
+
+    number_finished = data['number_finished']
+    with tqdm(total=number_finished, file=sys.stdout) as pbar:
+        while True:
+            task = get_task(id_token,task_id)
+            jsonData = task.json()['body']['data']
+            if jsonData['status'] == 'ERROR':
+                print('Currently your task is error, please check on https://dev.daita.tech/my-tasks')
+                footer()
+                break
+            number_gen_images = int(jsonData['number_gen_images']) 
+            pbar.update(number_gen_images)
+            if number_gen_images == number_finished:
+                break
+            time.sleep(2)
+    footer()
 
 def listCompressFiles(files):
     for index, file in enumerate(files):
